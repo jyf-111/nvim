7fb03a14edb9b47adda09346449cbc76e6c7d713
jyf <77335030+jyf-111@users.noreply.github.com>
2023-01-16 17:03:41 +0800

    .

--- lua/basic/keybinds.lua
+++ lua/basic/keybinds.lua
@@ -29,6 +29,10 @@ vim.keymap.set("n", "<F8>", "<cmd>AsyncTask run<CR>", { noremap = true, silent =
 -- basic
 vim.keymap.set("n", "<C-u>", "9k", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-d>", "9j", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-up>", "<cmd> res+5				<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-down>", "<cmd> res-5				<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-left>", "<cmd> vertical resize-5	<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-right>", "<cmd> vertical resize+5	<CR>", { noremap = true, silent = true })
 
 -- no hightlight
 vim.keymap.set("n", "<CR><CR>", "<cmd>noh<CR>", { noremap = true, silent = true })
